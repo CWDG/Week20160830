f701ecdf39bb7e1e332f97288aa042cdcf3a4847
johnsimer <johnsimerlink@gmail.com>
2016-09-06 19:38:35 -0400

    New message template and event handling

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,7 +5,26 @@ import { Messages } from '../api/messages.js'
 
 Template.body.helpers({
   messages () {
-    return Messages.find({});
+    return Messages.find({})
   }
-});
+})
 
+Template.newMessage.events({
+	'click #submitNewMessage' : function(template, events){
+		var newMessage = document.querySelector('#newMessage').value
+		, time = new Date()
+		, hours = time.getHours()
+		, minutes = time.getMinutes()
+		var newMessageObject = {
+			text: newMessage,
+			time: hours + ":" + minutes
+		}
+		Messages.insert(newMessageObject)
+		console.log("The message object we are inserting is", newMessageObject)
+	}
+})
+
+[
+{text: "hi", time: "8pm"},
+{text: "hello", time: "9pm"}
+]
