43a65f0e8203f071a1800fec5ca17528c141a735
johnsimer <johnsimerlink@gmail.com>
2016-09-06 20:30:41 -0400

    nickname and limit height

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,25 +6,40 @@ import { Messages } from '../api/messages.js'
 Template.body.helpers({
   messages () {
     return Messages.find({})
+    
   }
 })
 
 Template.newMessage.events({
 	'click #submitNewMessage' : function(template, events){
-		var newMessage = document.querySelector('#newMessage').value
+		var newMessageElement = document.querySelector('#newMessage')
+		, newMessage = newMessageElement.value
 		, time = new Date()
 		, hours = time.getHours()
 		, minutes = time.getMinutes()
+		, nickname = document.querySelector('#nickname').value;
+		if (!isMessageAllowed(newMessage)){
+			alert('Make your message shorter or longer')
+			return;
+		}
 		var newMessageObject = {
 			text: newMessage,
-			time: hours + ":" + minutes
+			time: hours + ":" + minutes,
+			nickname: nickname
 		}
 		Messages.insert(newMessageObject)
 		console.log("The message object we are inserting is", newMessageObject)
+		newMessageElement.value = ""
+
+
 	}
 })
 
+function isMessageAllowed(message){
+	return message.length <= 140 && message.length > .42
+}
+
 [
-{text: "hi", time: "8pm"},
+{text: "hi", time: "8pm", name: "Harambe"},
 {text: "hello", time: "9pm"}
 ]
